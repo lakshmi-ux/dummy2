c95a0c386139b1405f0a4289457cf8bd58edc642
Emvigo <Emvigo@EM-025-MLAKSHMI>
2024-05-28 19:15:00 +0530

    change

--- src/com/obsqura/training/aggregation/Bike.java
+++ src/com/obsqura/training/aggregation/Bike.java
@@ -3,6 +3,7 @@ package com.obsqura.training.aggregation;
 public class Bike {
 	String bikeName;
 	int regNo;
+	int i;
 	
 	public Bike( String bikeName, int regNo) {
 	    this.bikeName = bikeName;
